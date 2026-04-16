74f923f2cd48978060f5e383599f244a03a63406
alexagapkin <alexthemaggot23@gmail.com>
2026-04-17 02:21:52 +0500

    - move api urls to api_urls file

--- uznuts_track_server/api/urls.py
+++ uznuts_track_server/api/urls.py
@@ -1,10 +1,7 @@
-from django.urls import path
-
-from .views import create_employee, ingest_location, list_positions, update_employee
+from django.urls import path, include
+from . import views
 
 urlpatterns = [
-    path("employees/", create_employee, name="create_employee"),
-    path("employees/<int:employee_id>/", update_employee, name="update_employee"),
-    path("positions/", list_positions, name="list_positions"),
-    path("locations/", ingest_location, name="ingest_location"),
+    path("api/", include("api.api_urls")),
+    path('', views.index)
 ]
